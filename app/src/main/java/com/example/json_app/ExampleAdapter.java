d199090ba12716a25bd9a525b32de507d8cd719c
babamyrat <-l>
2021-07-05 19:56:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/json_app/ExampleAdapter.java
@@ -0,0 +1,105 @@
+package com.example.json_app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.json_app.ExampleItem;
+
+import java.util.List;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<ExampleItem> mExampleItem;
+    private RecyclerViewClickListener listener;
+
+    public ExampleAdapter(Context mContext, List<ExampleItem> mExampleItem, RecyclerViewClickListener listener) {
+        this.mContext = mContext;
+        this.mExampleItem = mExampleItem;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        v = inflater.inflate(R.layout.movie_item, parent, false);
+
+        return  new MyViewHolder(v);
+
+
+    }
+
+
+
+
+
+    @Override
+    public void onBindViewHolder(@NonNull  ExampleAdapter.MyViewHolder holder, int position) {
+
+
+        holder.name.setText(mExampleItem.get(position).getName());
+        holder.species.setText(mExampleItem.get(position).getSpecies());
+        holder.gender.setText(mExampleItem.get(position).getHogwartsStudent());
+
+        // using Glide
+        Glide.with(mContext)
+                .load(mExampleItem.get(position).getImg())
+                .into(holder.img);
+
+        holder.wood.setText(mExampleItem.get(position).getWood());
+        holder.length.setText(mExampleItem.get(position).getLength());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleItem.size();
+
+    }
+
+    public interface RecyclerViewClickListener{
+        void onClick(View v, int position);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+
+        TextView name;
+        TextView species;
+        TextView gender;
+        ImageView img;
+        TextView wood;
+        TextView length;
+
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.name_txt);
+            species = itemView.findViewById(R.id.id_txt);
+            gender = itemView.findViewById(R.id.test_txt);
+            img = itemView.findViewById(R.id.imageView);
+            wood = itemView.findViewById(R.id.wood_txt);
+            length = itemView.findViewById(R.id.length_txt);
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            listener.onClick(itemView, getAdapterPosition());
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/json_app/ExampleItem.java
@@ -0,0 +1,235 @@
+package com.example.json_app;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ExampleItem implements Parcelable {
+
+    String name;
+    String species;
+    String gender;
+    String house;
+    String dateOfBirth;
+    String yearOfBirth;
+    String ancestry;
+    String eyeColour;
+    String hairColour;
+
+    String wood;
+    String core;
+    String length;
+
+    String patronus;
+    String hogwartsStudent;
+    String hogwartsStaff;
+    String actor;
+    String alive;
+    String img;
+
+    public ExampleItem(String name, String species, String gender, String house, String dateOfBirth,
+                       String yearOfBirth, String ancestry, String eyeColour, String hairColour,
+                       String wood, String core, String length, String patronus, String hogwartsStudent,
+                       String hogwartsStaff, String actor, String alive, String img) {
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.house = house;
+        this.dateOfBirth = dateOfBirth;
+        this.yearOfBirth = yearOfBirth;
+        this.ancestry = ancestry;
+        this.eyeColour = eyeColour;
+        this.hairColour = hairColour;
+        this.wood = wood;
+        this.core = core;
+        this.length = length;
+        this.patronus = patronus;
+        this.hogwartsStudent = hogwartsStudent;
+        this.hogwartsStaff = hogwartsStaff;
+        this.actor = actor;
+        this.alive = alive;
+        this.img = img;
+    }
+
+    public ExampleItem() {
+
+    }
+
+    protected ExampleItem(Parcel in) {
+        name = in.readString();
+        species = in.readString();
+        gender = in.readString();
+        img = in.readString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getAncestry() {
+        return ancestry;
+    }
+
+    public String getEyeColour() {
+        return eyeColour;
+    }
+
+    public String getHairColour() {
+        return hairColour;
+    }
+
+    public String getWood() {
+        return wood;
+    }
+
+    public String getCore() {
+        return core;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public String getHogwartsStudent() {
+        return hogwartsStudent;
+    }
+
+    public String getHogwartsStaff() {
+        return hogwartsStaff;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public String getAlive() {
+        return alive;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setYearOfBirth(String yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setAncestry(String ancestry) {
+        this.ancestry = ancestry;
+    }
+
+    public void setEyeColour(String eyeColour) {
+        this.eyeColour = eyeColour;
+    }
+
+    public void setHairColour(String hairColour) {
+        this.hairColour = hairColour;
+    }
+
+    public void setWood(String wood) {
+        this.wood = wood;
+    }
+
+    public void setCore(String core) {
+        this.core = core;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
+    public void setHogwartsStudent(String hogwartsStudent) {
+        this.hogwartsStudent = hogwartsStudent;
+    }
+
+    public void setHogwartsStaff(String hogwartsStaff) {
+        this.hogwartsStaff = hogwartsStaff;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public void setAlive(String alive) {
+        this.alive = alive;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+
+
+    public static final Parcelable.Creator<ExampleItem> CREATOR = new Creator<ExampleItem>() {
+        @Override
+        public ExampleItem createFromParcel(Parcel in) {
+            return new ExampleItem(in);
+        }
+
+        @Override
+        public ExampleItem[] newArray(int size) {
+            return new ExampleItem[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(species);
+        dest.writeString(gender);
+        dest.writeString(img);
+    }
+}
